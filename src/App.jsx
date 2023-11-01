ee2472abb1bd273627e9e9e1e0906f93e55716a5
Tushyen <tanmaygotmare@gmail.com>
2023-11-01 14:41:46 +0530

    this is tic tac toe game

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './styles.scss'
+// import Square from './components/Square'
+import Board from './components/Board'
+
+function App() {
+  
+
+  return (
+    
+    <div className='app'>
+    
+    <Board/>
+    </div>
+     
+  );
+}
+
+export default App;
