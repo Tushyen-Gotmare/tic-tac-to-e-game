da63c9d038b832dd50871e1dd145d3920995ff90
Tushyen <tanmaygotmare@gmail.com>
2023-11-02 09:09:09 +0530

    this is x

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,21 +1,45 @@
+import { useState } from "react";
 import Square  from "./Square.jsx";
 const Board = () => {
+   const [squares, setSquares] = useState(Array(9).fill(null));
+
+   const handleSquareClick = clickedPosition =>{
+
+      setSquares((currentSquares) => {
+
+         return currentSquares.map((sqaureValue, position) => {
+            if(clickedPosition === position){
+               return 'X';
+            }
+            return sqaureValue;
+
+         })
+      }
+      )
+
+   };
+   const renderSquare = position => {
+      return(
+         <Square value={squares[position]} onClick ={() => handleSquareClick(position)}/>
+      );
+   };
     return <>
     <div className="board">
        <div className="board-row">
-          <Square value={0}/>
-          <Square value={1}/>
-          <Square value={2}/>
+         {renderSquare(0)}
+         {renderSquare(1)}
+         {renderSquare(2)}
+         
        </div>
        <div className="board-row">
-       <Square value={3}/>
-       <Square value={4}/>
-       <Square value={5}/>
+       {renderSquare(3)}
+         {renderSquare(4)}
+         {renderSquare(5)}
        </div>
        <div className="board-row">
-       <Square value={6}/>
-       <Square value={7}/>
-       <Square value={8}/>
+       {renderSquare(6)}
+         {renderSquare(7)}
+         {renderSquare(8)}
        </div>
     </div>
     </>
